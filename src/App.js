319ffb8e069f71d90d50b27746c3ba989397f3e6
HebaSalem <puupiiiii@gmail.com>
2022-04-06 01:42:50 +0200

    edit app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router , Routes ,Roure, Route} from 'react-router-dom'
+import {BrowserRouter as Router , Routes , Route} from 'react-router-dom'
 import './App.css';
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
